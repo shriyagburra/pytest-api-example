291ffbdb5aed8d5612df42171fc6b62406226689
shriyagburra <shriyabur@gmail.com>
2026-03-09 15:31:14 -0400

    feature-shriyab Added test_pet changes and tests

--- test_pet.py
+++ test_pet.py
@@ -7,7 +7,7 @@ from hamcrest import assert_that, contains_string, is_
 
 '''
 TODO: Finish this test by...
-1) Troubleshooting and fixing the test failure
+1) Troubleshooting and fixing the test failure -- Complete
 The purpose of this test is to validate the response matches the expected schema defined in schemas.py
 '''
 def test_pet_schema():
@@ -22,26 +22,39 @@ def test_pet_schema():
 
 '''
 TODO: Finish this test by...
-1) Extending the parameterization to include all available statuses
-2) Validate the appropriate response code
-3) Validate the 'status' property in the response is equal to the expected status
-4) Validate the schema for each object in the response
+1) Extending the parameterization to include all available statuses -- Complete
+2) Validate the appropriate response code -- Complete
+3) Validate the 'status' property in the response is equal to the expected status -- Complete
+4) Validate the schema for each object in the response -- Complete
 '''
 @pytest.mark.parametrize("status", [("available")])
 def test_find_by_status_200(status):
+
     test_endpoint = "/pets/findByStatus"
     params = {
         "status": status
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    assert response.status_code == 200
+    data = response.json()
+
+    for pet in data:
+        assert pet["status"] == status
+        validate(instance=pet, schema=schemas.pet)
+    
 
 '''
 TODO: Finish this test by...
-1) Testing and validating the appropriate 404 response for /pets/{pet_id}
-2) Parameterizing the test for any edge cases
+1) Testing and validating the appropriate 404 response for /pets/{pet_id} -- complete
+2) Parameterizing the test for any edge cases -- complete
 '''
 def test_get_by_id_404():
-    # TODO...
-    pass
+    pet_id = 11111111
+    endpoint = f"/pets/{pet_id}"
+
+    response = api_helpers.get_api_data(endpoint)
+    assert response.status_code == 404
+
+    assert "not found" in response.text.lower()
+
